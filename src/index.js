8c0267b47516729d630892f7462c589ac32673e0
modgera <modgera@gmail.com>
2019-07-31 22:55:59 +0500

    file structure, wp config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/styles/pages/style.scss';
+
+$(document).ready(() => {
+    console.log('Ready!');
+  });
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const wpPages = require('./config/wp-pages');
+
+module.exports = env  => {
+  return {
+    entry: './src/index.js',
+    mode: 'development',
+
+    output: {
+      path: __dirname + "/build",
+      filename: 'bundle.js'
+    },
+
+    plugins: [
+
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: './src/views/pages/index.pug',
+          inject: true
+      }),
+
+      ...wpPages.pages(env),
+
+      new MiniCssExtractPlugin({
+        filename: 'public/styles/[name].[hash:7].bundle.css',
+        chunkFilename: '[id].css',
+      }),
+
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.$': 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+
+          {
+            test: /\.pug$/,
+            loader: "pug-loader",
+            options: {
+              pretty: true
+            }
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              'file-loader?name=./public/img/[name].[ext]'
+            ]
+          },
+
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+              'file-loader'
+            ]
+          },
+
+          {
+            test: /\.css$/,
+            use: [
+              env === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  sourceMap: true
+                },
+              },
+            ],
+          },
+
+          {
+            test: /\.scss$/,
+            use: [
+              env === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+              { loader: 'css-loader', options: { importLoaders: 1, sourceMap: true } }, // translates CSS into CommonJS
+              'sass-loader', // compiles Sass to CSS
+            ],
+          },
+        ]
+    }
+  };
+}
